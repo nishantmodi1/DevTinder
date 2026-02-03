cbbcd9a27e09f2e240706d65d73bfe2af76cb186
nishant.m <nishant.m@ninestars.in>
2026-02-03 22:22:53 +0530

    multiple_route_handler

--- app.js
+++ app.js
@@ -2,30 +2,46 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.json());
+// app.use("/route", rH1, rH2, rH3, ...)
+// app.use("/route", [rH1, rH2, rH3])
+// app.use("/route", [rH1, rH2], rH3)
+
+// we can also send array of function
+app.use("/user", 
+  [(req, res, next) => {
+    console.log('kfdsj')
+    res.send("route handler 1")
+    next()
+  },
+  (req, res) => {
+    console.log('kfdsj')
+    res.send("route handler 2")
+  }],
+);
 
 // this will only handle to get call to /user
-app.get('/user', (req, res) => {
-  res.send({ first_name: 'Nishant', last_name: 'Modi'})
-})
+// app.get('/user', (req, res) => {
+//   res.send({ first_name: 'Nishant', last_name: 'Modi'})
+// })
 
+// /ac, /abc this is come in express version 4
 // params
 // app.get('/a/:b?/c', (req, res) => {
 //   res.send({ first_name: 'Nishant', last_name: 'Modi'})
 // })
 
-app.post('/user', (req, res) => {
-  res.send('post data is successfully!');
-});
-app.delete('/user/:userId', (req, res) => {
-  console.log(req.params)
-  res.send('delete data is successfully!');
-});
+// app.post('/user', (req, res) => {
+//   res.send('post data is successfully!');
+// });
+// app.delete('/user/:userId', (req, res) => {
+//   console.log(req.params)
+//   res.send('delete data is successfully!');
+// });
 
 //this will match all the http method api
-app.use('/test', (req, res) => {
-  res.send('hellow from the server coming!!')
-})
+// app.use('/test', (req, res) => {
+//   res.send('hellow from the server coming!!')
+// })
 
 app.listen(8000, () => {
   console.log('server is listening in port 3000...')
