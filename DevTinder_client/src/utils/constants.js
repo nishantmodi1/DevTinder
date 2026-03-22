b5ea7d5bb089b3e28ff5c3c2bf42c9b3a08914d6
nishant.m <nishant.m@ninestars.in>
2026-03-22 18:10:54 +0530

    l

--- DevTinder_client/src/utils/constants.js
+++ DevTinder_client/src/utils/constants.js
@@ -1,4 +1,4 @@
 // export const BASE_URL = "/api"
 // export const BASE_URL = "http://localhost:8000/"
 export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:8000/" : "/api/";
+  location.hostname === "localhost" ? "http://localhost:8000" : "/api";
